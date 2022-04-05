fe42a1e28e62e62b6083de6e7006ee5e4e08fcbe
teczekwoj <92214980+teczekwoj@users.noreply.github.com>
2022-04-05 21:13:42 +0200

    Add files via upload
    Upload Lab2

---
+++ Aplikacje-Backendowe/Lab2/src/main/java/com/example/demo/UsersController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+import java.util.Map;
+
+@Controller
+public class UsersController {
+
+    private final Map<Long, UserEntity> users = new HashMap<>();
+
+    @PostConstruct
+    private final void Create(){
+
+        users.put(1L, new UserEntity(1L, "Stanislaw"));
+        users.put(2L, new UserEntity(2L, "Sylwia"));
+        users.put(3L, new UserEntity(3L, "Jurek"));
+    }
+
+    @RequestMapping("/users/{id}/get")
+    @ResponseBody
+
+    public Object GetUser(
+            @PathVariable Long id
+            ){
+        return users.get(id);
+    }
+
+}
+
