017598e16189091d4bc6429a48bbec9ed7e311db
Wojciech Latko <wojciech.latko@microsoft.wsei.edu.pl>
2022-04-06 15:07:34 +0200

    add get all users

--- Aplikacje-Backendowe/Lab2/src/main/java/com/example/demo/UsersController.java
+++ Aplikacje-Backendowe/Lab2/src/main/java/com/example/demo/UsersController.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +15,31 @@ public class UsersController {
     private final Map<Long, UserEntity> users = new HashMap<>();
 
     @PostConstruct
-    private final void Create(){
+    private final void Create() {
 
         users.put(1L, new UserEntity(1L, "Stanislaw"));
         users.put(2L, new UserEntity(2L, "Sylwia"));
         users.put(3L, new UserEntity(3L, "Jurek"));
     }
 
+    //Get one user by ID
     @RequestMapping("/users/{id}/get")
     @ResponseBody
 
-    public Object GetUser(
+    public Object GetUserById(
             @PathVariable Long id
-            ){
+    ) {
         return users.get(id);
     }
 
+    //Get all users
+    @RequestMapping("/users")
+    @ResponseBody
+
+    public Object GetUsers(
+
+    ) {
+        return users;
+    }
 }
 
